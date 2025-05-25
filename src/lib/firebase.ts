b132a1ddc3e7f7eb56a83b1e0db7b0ea15aacc7f
atharvt21OPJU <atha.bt21cs18@opju.ac.in>
2025-05-26 02:05:46 +0530

    Update firebase.ts

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -3,13 +3,13 @@ import { initializeApp } from 'firebase/app'
 import { getAuth, RecaptchaVerifier, signInWithPhoneNumber } from 'firebase/auth';
 
 const firebaseConfig = {
-    apiKey: " ",
-    authDomain: " ",
-    projectId: " ",
-    storageBucket: " ",
-    messagingSenderId: " ",
-    appId: " ",
-    measurementId: " "
+    apiKey: "AIzaSyBO_wA3B0SIOSYI_708YXof-VCQgZftX1k",
+    authDomain: "vidya-medical-store.firebaseapp.com",
+    projectId: "vidya-medical-store",
+    storageBucket: "vidya-medical-store.firebasestorage.app",
+    messagingSenderId: "233743943436",
+    appId: "1:233743943436:web:ffd1c4ee3e64b9ea1d6b29",
+    measurementId: "G-828B9JM6R2"
   };
 
 const app = initializeApp(firebaseConfig)
